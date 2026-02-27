7fb1999490e0c6a7f41d192663a043b6ac4331bd
Atharv Mayur Tambekar <110536276+Atharv-28@users.noreply.github.com>
2026-02-27 10:41:58 +0530

    deployment issue fixed

--- src/hooks/useParticipants.js
+++ src/hooks/useParticipants.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { db } from '../firebase';
 import { collection, query, orderBy, getDocs } from 'firebase/firestore';
 
